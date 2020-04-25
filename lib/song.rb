ac5caae4bbd20ce9961f470e6e51245f7862b916
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-25 00:08:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Song
   
-  # extend Memorable
+  extend Memorable
   
   attr_accessor :name
   attr_reader :artist
@@ -21,13 +21,13 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
